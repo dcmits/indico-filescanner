d9cc14d494c5ad86ea4f41785774e54887e9ff26
Giorgio Pieretti <giorgio.pieretti@openprojects.it>
2018-07-26 11:48:45 +0200

    Check for pyclamd at start time

--- indicofilescanner/add.py
+++ indicofilescanner/add.py
@@ -3,7 +3,13 @@ from indico.core.logger import Logger
 
 
 def do_add():
-
+    # Do not let file upload if the ClamAV check is not available
+    try:
+        import pyclamd
+        pyclamd.ClamdAgnostic()
+        Logger.get('FileScanner').info("ClamAV daemon found.")
+    except:
+        Logger.get('FileScanner').critical("Unable to import pyclamd. No MATERIAL will be UPLOADED.")
 
     def monkey_setErrorList(fn):
         def new_funct(*args, **kwargs):
@@ -15,12 +21,11 @@ def do_add():
             try:
                 import pyclamd
                 pyclamd.ClamdAgnostic()
-                Logger.get('FileScanner').error("ClamAV daemon found.")
             except ValueError as e:
-                Logger.get('FileScanner').critical(e)
+                Logger.get('FileScanner').error(e)
                 self._errorList.append("File upload FAILED: Unable to check for Viruses: %s" % (e))
             except:
-                Logger.get('FileScanner').critical("Unable to import pyclamd. No files will be scanned.")
+                Logger.get('FileScanner').error("Unable to import pyclamd. No files will be scanned.")
                 self._errorList.append("File upload FAILED: Unable to check for Viruses.")
             else:
                 # If no other errors has been found, check for viruses
